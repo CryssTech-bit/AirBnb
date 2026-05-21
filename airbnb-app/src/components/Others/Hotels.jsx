0e1431eabd038950ad83fe47309fef94308e5fac
CryssTech-bit <crystalsquebe@gmail.com>
2026-05-21 17:09:46 +0100

    added hotel component

---
+++ airbnb-app/src/components/Others/Hotels.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const hotels = () => {
+  return (
+    <div className="bg-black">
+      <h1 className="text-black"> Hotels</h1>
+    </div>
+  );
+};
+
+export default hotels;
